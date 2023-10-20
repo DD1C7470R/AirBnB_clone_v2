0f94b24c28f5709f7655a54682174903694375d7
DD1C74707 <gracevalley101@gmail.com>
2023-10-20 05:55:48 -0700

    Add flask setup

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''a script that starts a Flask web application:'''
+
+from web_flask import app
+
+@app.route('/')
+def hello_bnb():
+    '''returns hello bnb'''
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=4000, debug=True)
