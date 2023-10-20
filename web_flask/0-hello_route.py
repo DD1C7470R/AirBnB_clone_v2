4ed8c510376fbad841d9d98135f07c5e7e5cc72c
DD1C74707 <gracevalley101@gmail.com>
2023-10-20 06:45:35 -0700

    Update task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,8 +3,10 @@
     Write a script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:'''
+from flask import Flask
 
-from web_flask import app
+
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
