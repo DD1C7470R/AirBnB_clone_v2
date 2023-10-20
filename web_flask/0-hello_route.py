feab40e48b9007771da550e982fa3f8daa999602
DD1C74707 <gracevalley101@gmail.com>
2023-10-20 06:07:25 -0700

    Fix code styles

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,12 @@
 
 from web_flask import app
 
+
 @app.route('/')
 def hello_bnb():
     '''returns hello bnb'''
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
